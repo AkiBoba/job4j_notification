fca49a5accdb6a11ad465b9d274b918a89c273e5
Vladimir Likhachev <lettervl@ya.ru>
2023-05-03 23:49:16 +0300

    5. Повествование основанное на хореографии [#458498]

--- src/main/java/ru/job4j_notification/service/OrderService.java
+++ src/main/java/ru/job4j_notification/service/OrderService.java
@@ -1,23 +1,22 @@
 package ru.job4j_notification.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import ru.job4j_notification.model.OrderStatus;
 import ru.job4j_notification.repository.OrderRepository;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository repository;
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-
     @KafkaListener(topics = "preorder")
-    public void receiveOrder(String newOrder) throws JsonProcessingException {
-        OrderStatus orderStatus = OBJECT_MAPPER.readValue(newOrder, OrderStatus.class);
-        repository.save(orderStatus);
+    public void receiveOrder(OrderStatus newOrder) {
+        repository.save(newOrder);
+        log.info("Получен и сохранен ордер {}", newOrder);
+
     }
 }
